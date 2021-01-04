9824f9ac690f912cebfe99006e307c436b61d922
Joel <jbirch88655@gmail.com>
2021-01-04 05:31:03 -0800

    added user preferences

--- src/routes/web.php
+++ src/routes/web.php
@@ -26,4 +26,24 @@ Route::prefix('api/reacttour')->group(function () {
         userfinishedtraining::updateOrCreate(['user_id' => $userprofile->Get_User_Oid($request),'training_id' => trainingstep::where('name',$request->input('name'))->firstOrFail()->id]);
         return response()->json(["message"=>"User Training Step Complete"]);
     });
+    Route::get('userpreferences', function () {
+        $id = new AzureAuth;
+        $user_pref = user_preference::where('oid',$id->Get_User_Oid(request()))->get();
+        return response()->json(['message' => 'current user preferences', 'userpreferences' => $user_pref]);
+    });
+    Route::post('userpreferences', function () {
+        request()->validate(['preference' => 'required|string|max:255','value' => 'required|string|max:255']);
+        $id = new AzureAuth;
+        $user_pref = new user_preference;
+        $user_pref->preference = request()->input('preference');
+        $user_pref->oid = $id->Get_User_Oid(request());
+        $user_pref->value = request()->input('value');
+        $user_pref->save();
+        return response()->json(['message' => 'current user preferences', 'userpreferences' => $user_pref]);
+    });
+    Route::put('userpreferences/{user_preference}', function (App\Models\user_preference $user_preference) {
+        request()->validate(['value' => 'required|string|max:255']);
+        $user_preference->value = request()->input('value');
+        return response()->json(['message' => 'current user preferences', 'userpreferences' => $user_preference]);
+    });
 });
